0b1164ed06880e81f15a0e9f8ccd4461654c47a5
Deneuville Fabian <deneuville.fabian@live.fr>
2022-04-10 16:51:41 +0200

    Creating backend folder structure

---
+++ backend/server.js
@@ -0,0 +1,57 @@
+/******************** NODE SERVER CONFIGURATION ********************/
+
+/* Importing node HTTP package */
+const http = require('http');
+
+/* Importing express application */
+const app = require('./app');
+
+/* Creating a normalizePort function that returns a valid port, whether supplied as a number or a string */
+const normalizePort = val => {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+/* Creating an errorHandler function that checks for errors and handles them appropriately */
+const errorHandler = error => {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges.');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use.');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+/* Creating server using the createServer method from the node http package and setting the express application as parameter */
+const server = http.createServer(app);
+
+/* Saving the errorHandler function in the server */
+server.on('error', errorHandler);
+server.on('listening', () => {
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+  console.log('Listening on ' + bind);
+});
+
+/* Using the listen method from server to listen to port as defined */
+server.listen(port);
