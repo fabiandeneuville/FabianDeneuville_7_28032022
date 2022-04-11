fd1a985c04b3cb9b90e47d186d825a462ec3dff8
Deneuville Fabian <deneuville.fabian@live.fr>
2022-04-11 21:12:58 +0200

    Working on signup and login - (inputs validation, email encryption, password hashing + authentication\'s token)

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -3,17 +3,127 @@
 /* Importing db connection configuration  */
 const mysql = require('../dbConnection').connection;
 
-/* SIGNUP */
+/* Importing jsonwebtoken */
+const jwt = require('jsonwebtoken');
+
+/* Importing environment variables */
+require('dotenv').config();
+const EMAIL_ENCRYPTION_KEY = process.env.EMAIL_ENCRYPTION_KEY;
+const JWT_SECRET_TOKEN = process.env.JWT_SECRET_TOKEN;
+
+/***** SECURITY *****/
+
+/* Importing bcrypt */
+const bcrypt = require('bcrypt');
+
+/* Importing crypto-js */
+const cryptoJs = require('crypto-js')
+
+/* Importing email-validator */
+const emailValidator = require('email-validator');
+
+/* Importing password-validator and creating schema */
+const passwordValidator = require('password-validator');
+const schema = new passwordValidator();
+schema
+.is().min(8)
+.is().max(20)
+.has().uppercase()
+.has().lowercase()
+.has().digits()
+.has().not().spaces()
+.has().symbols()
+
+/* Creating a regex to check the validity of the req inputs to avoid injections */
+const regexInputs = /^[a-zA-Z0-9 _.,!()&éêèàçùîï]+$/;
+
+/***** SIGNUP *****/
 exports.signup = (req, res, next) => {
-    const userName = req.body.userName;
+    const username = req.body.username;
     const email = req.body.email;
     const password = req.body.password;
-    mysql.query(`INSERT INTO user (username, email, password) VALUES (?,?,?)`, [userName, email, password], (err, result, fields) => {
-        if(err){
-            return res.status(500).json({err});
-        }
-        res.status(200).json({message: 'utilisateur créé !'})
+    const imageUrl = req.body.imageUrl;
+    const bio = req.body.bio;
+    if(!emailValidator.validate(email)){
+        return res.status(500).json({message: "adresse email NON valide !"});
+    } else if (!schema.validate(password)){
+        return res.status(500).json({message: "mot de passe NON valide. Utilisez des majuscules, minuscules, chiffres et symboles, aucun espace, pour 8(min) à 16(max) caractères"})
+    } else if (!regexInputs.test(bio)){
+        return res.status(500).json({message: "la bio est renseignée avec des caractères invalides !"})
+    }
+    const cryptedEmail = cryptoJs.SHA256(email, EMAIL_ENCRYPTION_KEY).toString();
+    bcrypt.hash(password, 10)
+    .then(hash => {
+        mysql.query(`INSERT INTO user (username, email, password, imageUrl, bio) VALUES (?,?,?,?,?)`, [username, cryptedEmail, hash, imageUrl, bio], (err, result, fields) => {
+            if(err){
+                return res.status(500).json({err});
+            }
+            res.status(200).json({message: 'utilisateur créé !'})
+        })
     })
 };
 
-/* LOGIN */
+/***** LOGIN *****/
+exports.login = (req, res, next) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    const cryptedEmail = cryptoJs.SHA256(email, EMAIL_ENCRYPTION_KEY).toString();
+    mysql.query(`SELECT * FROM user WHERE email = '${cryptedEmail}'`, (err, result, fields) => {
+        if(err){
+            return res.status(404).json({err});
+        }
+        bcrypt.compare(password, result[0].password)
+        .then(valid => {
+            if(!valid){
+                return res.status(401).json({message: "mot de passe incorrect !"})
+            }
+            res.status(200).json({
+                userId: result[0].id,
+                token: jwt.sign(
+                    {userId: result[0].id},
+                    JWT_SECRET_TOKEN,
+                    {expiresIn: '24h'}
+                )
+            })
+        })
+        .catch(error => res.status(500).json({error}));
+    });
+};
+
+/***** MODIFY USER *****/
+exports.modifyUser = (req, res, next) => {
+
+};
+
+/***** GET ALL USERS *****/
+exports.getAllUsers = (req, res, next) => {
+    mysql.query(`SELECT * FROM user`, (err, result, fields) => {
+        if(err){
+            return res.status(500).json({err});
+        }
+        res.status(200).json(result);
+    })
+};
+
+/***** GET ONE USER *****/
+exports.getOneUser = (req, res, next) => {
+    let userId = req.params.id;
+    mysql.query(`SELECT * FROM user WHERE id = ${userId}`, (err, result, fields) => {
+        if(err){
+            return res.status(404).json({message: "utilisateur introuvable !"});
+        }
+        return res.status(200).json(result);
+    })
+}
+
+/***** DELETE ONE USER *****/
+exports.deleteOneUser = (req, res, next) => {
+    let userId = req.params.id;
+    mysql.query(`DELETE FROM user WHERE id = ${userId}`, (err, result, fields) => {
+        if(err){
+            return res.status(404).json({message: "utilisateur introuvable"});
+        }
+        return res.status(200).json({message: "utilisateur supprimé !"});
+    })
+};
+
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -9,8 +9,13 @@ const router = express.Router();
 /* Importing user controller */
 const userControl = require('../controllers/user');
 
-/* Creating the signup and login routes */
+/* Creating the user routes */
 router.post('/signup', userControl.signup);
+router.post('/login', userControl.login);
+router.put('/:id', userControl.modifyUser)
+router.get('/', userControl.getAllUsers);
+router.get('/:id', userControl.getOneUser);
+router.delete('/:id', userControl.deleteOneUser);
 
 /* Exporting router */
 module.exports = router;
