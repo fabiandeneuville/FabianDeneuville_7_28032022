eb08db3c6629be41f02e24431ce2682d6d87cde7
Deneuville Fabian <deneuville.fabian@live.fr>
2022-05-04 21:49:53 +0200

    Working on connection

---
+++ frontend/src/views/Connection.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <img class="connexion__page__logo" src="../assets/icon-left-font-monochrome-black.png" alt="Logo de Groupomania">
+        <login> </login>
+    </div>
+</template>
+
+
+
+<script>
+
+    import Login from '../components/Login'
+
+    export default {
+        name: 'connection',
+        components: {
+            'login' : Login
+        }
+    }
+
+</script>
+
+
+<style>
+
+    .connexion__page__logo {
+        width:60%;
+        max-width: 600px;
+    }
+
+</style>
