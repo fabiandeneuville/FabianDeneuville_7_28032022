23574652b594cbdb95def40236c1b86f0298f101
Deneuville Fabian <deneuville.fabian@live.fr>
2022-05-04 09:09:02 +0200

    Initializing frontend folder

---
+++ frontend/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img class="general-logo" alt="logo de Groupomania" src="./assets/icon-left-font-monochrome-black.png">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Roboto', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.general-logo {
+  width:60%;
+  max-width: 600px;
+}
+</style>
