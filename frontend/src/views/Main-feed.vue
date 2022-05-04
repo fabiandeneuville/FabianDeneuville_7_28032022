20a5dd95e5e191f5c54c2c8f65d74a3e9c59311d
Deneuville Fabian <deneuville.fabian@live.fr>
2022-05-04 21:50:20 +0200

    Working on main feed

---
+++ frontend/src/views/Main-feed.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <headerTop> </headerTop>
+    </div>
+
+</template>
+
+<script>
+
+    import Header from '../components/Header.vue'
+
+    export default {
+        name: 'MainFeed',
+        data(){
+            return {
+                
+            }
+        },
+        components: {
+            'headerTop' : Header
+        },
+        methods: {
+            
+        }
+    }
+
+</script>
+
+
+
+<style>
+
+
+
+</style>
