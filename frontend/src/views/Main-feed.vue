58d88ed10bd05edfefd573fe09ad71e3d8928566
Deneuville Fabian <deneuville.fabian@live.fr>
2022-05-05 16:43:45 +0200

    Getting user infos from localstorage

--- frontend/src/views/Main-feed.vue
+++ frontend/src/views/Main-feed.vue
@@ -1,26 +1,61 @@
 <template>
     <div>
         <headerTop> </headerTop>
+        <postForm 
+            v-bind:username="username" 
+            v-bind:imageUrl="imageUrl">
+        </postForm>
     </div>
 
 </template>
 
 <script>
 
-    import Header from '../components/Header.vue'
+    import axios from 'axios'
+
+    import Header from '../components/Header'
+    import PostForm from '../components/PostForm'
 
     export default {
         name: 'MainFeed',
         data(){
             return {
-                
+                userId: null,
+                role: null,
+                token: null,
+                username: null,
+                imageUrl: null
             }
         },
+        mounted: function(){
+            this.fromLocalStorage()
+            this.getOneUser()
+        },
         components: {
-            'headerTop' : Header
+            'headerTop' : Header,
+            'postForm' : PostForm
         },
         methods: {
-            
+            fromLocalStorage: function(){
+                const user = JSON.parse(localStorage.getItem('user'))
+                this.userId = user.userId
+                this.role = user.role
+                this.token = user.token
+            },
+            getOneUser: function(){
+
+                const config = {
+                    headers: { Authorization: `Bearer ${this.token}` }
+                }
+
+                axios
+                .get(`http://localhost:3000/api/user/${this.userId}`, config)
+                .then(response => {
+                    this.username = response.data.username
+                    this.imageUrl = response.data.imageUrl
+                })
+
+            }
         }
     }
 
@@ -28,8 +63,28 @@
 
 
 
-<style>
+<style scoped>
 
+button {
+    position: absolute; 
+    top:50%;
+}
+p {
+    position: absolute; 
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    font-size: 50px;
+    background: white;
+    padding:30px;
+}
 
+img {
+    position: absolute;
+    top:60%;
+    width:50px;
+    height:50px;
+    object-fit: cover;
+}
 
 </style>
