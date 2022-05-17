999f4f0fd5496d11d4b627a40817b8a531e647b6
Deneuville Fabian <deneuville.fabian@live.fr>
2022-05-17 10:46:19 +0200

    Adding the user view to display user details and user posts (not implemented yet)

--- frontend/src/views/User.vue
+++ frontend/src/views/User.vue
@@ -1,9 +1,16 @@
 <template>
     <div>
         <headerTop></headerTop>
-
-
-
+        <div class="user__container">
+            <h3 class="user__heading">Profile de {{ username }}</h3>
+            <div class="user__img__container">
+                <img v-bind:src="this.imageUrl" v-bind:alt="`Photo de ${this.username}`">
+            </div>
+            <p class="user__bio">{{ bio }}</p>
+            <a v-if="this.loggedUserId != this.userId" class="user__contact" v-bind:href="`mailto:${this.email}`">
+                <i class="fa-solid fa-envelope"></i>
+            </a>
+        </div>
         <footerBottom></footerBottom>
     </div>
 </template>
@@ -14,25 +21,100 @@
 import Header from '../components/Header.vue'
 import Footer from '../components/Footer.vue'
 
+import axios from 'axios'
+
 export default {
     name:'User',
     data(){
         return {
-            userId: this.$route.params.id
+            userId: this.$route.params.id,
+            token: undefined,
+            username: undefined,
+            bio: undefined,
+            email: undefined,
+            imageUrl: undefined,
+            loggedUserId: undefined,
         }
     },
     components: {
         'headerTop': Header,
         'footerBottom': Footer
     },
+    created(){
+        this.token = JSON.parse(localStorage.getItem('user')).token;
+        this.loggedUserId = JSON.parse(localStorage.getItem('user')).userId;
+    },
     mounted(){
-        console.log(this.userId)
+        const config = {
+            headers: { Authorization: `Bearer ${this.token}` }
+        }
+        axios
+        .get(`http://localhost:3000/api/user/${this.userId}`, config)
+        .then(response => {
+            this.username = response.data.username;
+            this.bio = response.data.bio;
+            this.email = response.data.email;
+            this.imageUrl = response.data.imageUrl;
+        })
+        .catch(error => {
+            console.log(error)
+        })
     }
 }
 </script>
 
 <style>
 
+    .user__container {
+        margin:110px auto 0 auto;
+        width:95%;
+        max-width: 600px;
+        background: white;
+        padding:15px;
+        border-radius:5px;
+        box-shadow: 2px 5px 10px 0px #333;
+    }
+
+    .user__heading {
+        margin:10px auto 20px auto;
+        padding-bottom:5px;
+        border-bottom: 1px solid #333;
+    }
+
+    .user__img__container {
+        width:200px;
+        height:200px;
+        margin: 10px auto 10px auto;
+        overflow: hidden;
+        border-radius:50%;
+        border:4px solid rgb(233, 68, 37);
+    }
+
+    .user__img__container img {
+        width:100%;
+        height:100%;
+        object-fit: cover;
+    }
+
+    .user__bio {
+        margin:10px auto 10px auto;
+        padding:10px;
+        text-align: justify;
+        border-bottom: 1px solid #333;
+    }
+
+    .user__contact {
+        display: inline-block;
+        padding:15px;
+        color: #333;
+        font-size: 24px;
+        transition: all 0.3s ease;
+    }
+
+    .user__contact:hover {
+        color: rgb(233, 68, 37);
+        transform: scale(1.2)
+    }
 
 
 </style>
