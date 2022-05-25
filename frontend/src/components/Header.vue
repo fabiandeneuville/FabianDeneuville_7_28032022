6e9c6d68d1b6f5e1f4807f563ccfe3d5164edbfb
Deneuville Fabian <deneuville.fabian@live.fr>
2022-05-25 08:58:01 +0200

    Working on style update with sass - Header done

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,12 +1,12 @@
 /********** HEADER **********/
 
 <template>
-    <div class="header-top">
+    <div class="header">
         <router-link to="/fil" class="header__logo__desktop__container">
-            <img class="logo__desktop" src="../assets/icon-left-font-monochrome-white.png" alt="logo de Groupomania">
+            <img class="header__logo__desktop" src="../assets/icon-left-font-monochrome-white.png" alt="logo de Groupomania">
         </router-link>
         <router-link to="/fil" class="header__logo__mobile__container">
-            <img class="logo__mobile" src="../assets/icon-white.png" alt="logo de Groupomania">
+            <img class="header__logo__mobile" src="../assets/icon-white.png" alt="logo de Groupomania">
         </router-link>
         <router-link v-if="location !== '/profile'" to="/profile" role="button" tabindex=0 class="header__link">
             <i class="fa-solid fa-user"></i>
@@ -55,100 +55,3 @@
     }
 
 </script>
-
-<style scoped>
-
-    .header-top {
-        position: fixed;
-        z-index:9999;
-        top:0px;
-        width: 100%;
-        height:100px;
-        padding: 10px;
-        display:flex;
-        flex-direction: row;
-        justify-content: space-between;
-        align-items: center;
-        background: #333;
-        overflow: hidden;
-    }
-
-    .header__logo__desktop__container {
-        display: block;
-        width:50%;
-    }
-
-    .header__logo__mobile__container {
-        display:none;
-    }
-
-    .logo__desktop {
-        width: 100%;
-        max-width: 500px;
-    }
-
-    .header__link {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        width:25%;
-        cursor: pointer;
-        transition: all 0.2s ease;
-        text-decoration: none;
-    }
-
-    .header__link:hover {
-        transform: scale(1.2);
-    }
-
-    .header__link i {
-        color: white;
-        font-size: 25px;
-    }
-
-
-    .tooltiptext {
-        display: inline-block;
-        opacity: 0;
-        margin-top: 5px;
-        font-size: 14px;
-        color: white;
-        transition: all 0.3s ease;
-    }
-
-    .header__link:hover .tooltiptext {
-        visibility: visible;
-        opacity: 1;
-    }
-    
-    @media screen and (max-width:900px){
-    
-            
-        .header-top {
-            justify-content: space-around;
-        }
-        
-        .header__logo__desktop__container {
-            display: none;
-        }
-    
-        .header__logo__mobile__container {
-            display: block; 
-            width: 33%;
-        }
-    
-        .logo__mobile {
-            height: 80px;
-        }
-    
-        .header__link {
-            width: 33%;
-        }
-
-        .tooltiptext {
-            font-size: 10px;
-        }
-    }
-
-</style>
