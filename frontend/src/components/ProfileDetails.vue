e50124eae5eed5da1bf791b60687b3791ca7bf89
Deneuville Fabian <deneuville.fabian@live.fr>
2022-05-25 20:48:55 +0200

    Working on style update with sass - profile details done

--- frontend/src/components/ProfileDetails.vue
+++ frontend/src/components/ProfileDetails.vue
@@ -2,22 +2,22 @@
 
 <template>
     <div>
-        <div class="profile__container">
+        <div class="profile">
             <div class="profile__img__container">
                 <img class="profile__img" v-bind:src="imageUrl" v-bind:alt="`Photo de ${username}`">
             </div>
             <h3 class="profile__username">{{ username }}</h3>
             <p class="profile__bio">{{ bio }}</p>
-            <div class="edit__profile__btn__container">
-                <div v-on:click="editFormIsVisible = !editFormIsVisible, passwordFormIsVisible = false" class="btn edit-btn">
+            <div class="profile__management">
+                <div v-on:click="editFormIsVisible = !editFormIsVisible, passwordFormIsVisible = false" class="profile__management__btn">
                     <i class="fa-solid fa-pen-to-square"></i>
                     <span class="tooltiptext">Modifier le profil</span>
                 </div>
-                <div v-on:click="passwordFormIsVisible = !passwordFormIsVisible, editFormIsVisible = false" class="btn change-password-btn">
+                <div v-on:click="passwordFormIsVisible = !passwordFormIsVisible, editFormIsVisible = false" class="profile__management__btn">
                     <i class="fa-solid fa-gear"></i>
                     <span class="tooltiptext">Changer le mot de passe</span>
                 </div>
-                <div v-on:click="deleteProfile" class="btn delete-btn">
+                <div v-on:click="deleteProfile" class="profile__management__btn">
                     <i class="fa-solid fa-trash"></i>
                     <span class="tooltiptext">Supprimer le compte</span>
                 </div>
@@ -120,86 +120,3 @@ export default {
 }
 
 </script>
-
-<style scoped>
-
-    .profile__container {
-        width:95%;
-        max-width: 600px;
-        padding:15px;
-        background: white;
-        margin: 110px auto 0 auto;
-        display: flex; 
-        flex-direction: row;
-        flex-wrap: wrap;
-        justify-content: center;
-        align-items: center;
-        border-radius:5px;
-        box-shadow: 2px 5px 10px 0px #333;
-    }
-
-    .profile__img__container {
-        width:150px;
-        height:150px;
-        border-radius:50%;
-        border:4px solid rgb(233, 68, 37);
-        overflow: hidden;
-    }
-
-    .profile__img {
-        height:100%;
-        width: 100%;
-        object-fit: cover;
-    }
-
-    .profile__username {
-        width: 100%;
-        margin-top: 15px;
-    }
-
-    .profile__bio {
-        width:100%;
-        padding:20px;
-        text-align: justify;
-        border-bottom:1px solid rgb(233, 68, 37);
-    }
-
-    .edit__profile__btn__container {
-        width:100%;
-        display: flex;
-        flex-direction: row;
-    }
-
-    .btn {
-        width:50%;
-        padding:10px;
-        line-height: 20px;
-        display: flex;
-        flex-direction: column;
-    }
-
-    .btn i {
-        font-size: 25px;
-        cursor: pointer;
-        transition: all 0.3s ease;
-    }
-
-    .btn i:hover {
-        color: rgb(233, 68, 37);
-        transform: scale(1.2);
-    }
-
-    .tooltiptext {
-        color: rgb(233, 68, 37);
-        display: inline-block;
-        opacity: 0;
-        margin-top: 5px;
-        font-size: 14px;
-        transition: all 0.3s ease;
-    }
-
-    .btn:hover .tooltiptext {
-        opacity: 1;
-    }
-
-</style>
