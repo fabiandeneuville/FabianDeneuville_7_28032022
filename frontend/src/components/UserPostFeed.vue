54f1576dbc16c522be6547a37b6bdbb91c856bc7
Deneuville Fabian <deneuville.fabian@live.fr>
2022-05-31 23:09:58 +0200

    UserPostFeed action bug fixed

--- frontend/src/components/UserPostFeed.vue
+++ frontend/src/components/UserPostFeed.vue
@@ -5,7 +5,8 @@
         <h3 class="feed__heading">{{ apiResponseMessage }}</h3>
         <ul>
             <li v-bind:key="index" v-for="(post, index) in userPosts">
-                <post  
+                <post
+                @updatePostList="updatePostList"   
                 v-bind:id="post.id"
                 v-bind:title="post.title"
                 v-bind:content="post.content"
@@ -40,26 +41,34 @@ export default {
     components: {
         'post': Post
     },
-    mounted(){
-        
-        const config = {
-        headers: { Authorization: `Bearer ${this.token}` }
-        }
-        axios
-        .get(`http://localhost:3000/api/post/user/${this.userId}`, config)
-        .then(response => {
-            if(response.data.message === 'Aucun posts !'){
-                this.apiResponseMessage = 'Aucune publication !'
-            } else {
-                for (let post of response.data){
-                    this.userPosts.push(post);
-                    this.apiResponseMessage = 'Les publications :'
-                }
+    methods: {
+        getAllPosts: function(){
+            const config = {
+                headers: { Authorization: `Bearer ${this.token}` }
             }
-        })
-        .catch(error => {
-            console.log(error)
-        })
+            axios
+            .get(`http://localhost:3000/api/post/user/${this.userId}`, config)
+            .then(response => {
+                if(response.data.message === 'Aucun posts !'){
+                    this.apiResponseMessage = 'Aucune publication !'
+                } else {
+                    for (let post of response.data){
+                        this.userPosts.push(post);
+                        this.apiResponseMessage = 'Les publications :'
+                    }
+                }
+            })
+            .catch(error => {
+                console.log(error)
+            })
+        },
+        updatePostList: function(){
+            this.userPosts = []
+            this.getAllPosts()
+        }
+    },
+    mounted(){
+        this.getAllPosts()       
     },
 }
 
